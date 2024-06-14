e8e8894b0cbf7eedc61cd3df27ccf5803ba2a901
Artur <Artur.xchoyan.16@inbox.ru>
2024-06-14 21:13:25 +0400

    init

---
+++ turo/sortirowka.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Hayastan Yerevan,
+Vrastan Tbilisi,
+USA Vashington,
+Hunastan Atenq," >> ~/home/artur/sortirowka.sh
+
+sort ~/home/artur/sortirowka.sh >> ~/home/artur/sorted_fayl_zadaniya
+
+echo "Задание выполнено"
